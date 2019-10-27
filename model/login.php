c32fd8f0d528a28bf477d5b2a0d7159725d745ed
unknown <riyad298@gmail.com>
2019-10-28 01:33:52 +0600

    procedure working

--- model/login.php
+++ model/login.php
@@ -1,28 +1,38 @@
 <?php 
 include "../address.php";
 
-// MYSQLI
-$mysqli = new mysqli('localhost', 'root', '', $databaseName);
-print '<h3>MYSQLI: simple select</h3>';
-$rs = $mysqli->query( 'SELECT * FROM users;' );
-while($row = $rs->fetch_object())
-{
-debug($row);
-}
-print '<h3>MYSQLI: calling sp with out variables</h3>';
-$rs = $mysqli->query( 'CALL login("araf", "arefea" , @first)' );
-$rs = $mysqli->query( 'SELECT @first' );
-while($row = $rs->fetch_object())
-{
-debug($row);
-}
 
-function debug($o)
-{
-print '<pre>';
-print_r($o);
-print '</pre>';
-}
+    try {
+        $pdo = new PDO("mysql:host=$hostName;dbname=$databaseName", $userName, $password);
+ 
+        // calling stored procedure command
+        $sql = 'CALL login(:id , :pass , @level)';
+ 
+        // prepare for execution of the stored procedure
+        $stmt = $pdo->prepare($sql);
+ 		$customerNumber = 'aref';
+ 		$pass = 'rf' ; 
+        // pass value to the command
+        $stmt->bindParam(':id', $customerNumber, PDO::PARAM_STR);
+        $stmt->bindParam(':pass', $pass, PDO::PARAM_STR);
+
+ 
+        // execute the stored procedure
+        $stmt->execute();
+ 
+        $stmt->closeCursor();
+ 
+        // execute the second query to get customer's level
+        $row = $pdo->query("SELECT @level AS level")->fetch(PDO::FETCH_ASSOC);
+        print_r($row);
+        if ($row) {
+        	echo  $row['level'];
+            return $row !== false ? $row['level'] : null;
+        }
+    } catch (PDOException $e) {
+        die("Error occurred:" . $e->getMessage());
+    }
+    
 
 
 
