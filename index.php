5a902e40e74e870ec15d364891f486c4c65f8d5f
Riyad Ahsan <riyad298@gmail.com>
2019-11-01 01:07:57 +0600

    revert vue cli

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1"><title>riyassss</title><link href=dist/static/css/app.30790115300ab27614ce176899523b62.css rel=stylesheet></head>
+<?php 
+include "assets\linker\linkerCss.php" ; 
+?>
 
-<body>
-	<div id=app>
-		
-		<hi>
-	</div>
-	<script type=text/javascript src=dist/static/js/manifest.37a2ecbb1d1b7e6c9ada.js>
-		
-	</script><script type=text/javascript src=dist/static/js/vendor.ae80dd2d4209727aff36.js></script><script type=text/javascript src=dist/static/js/app.e0ad3a21dc8be8363de8.js></script>
-</body>
-</html>
+
+<h1>hi</h1>
+
+
+<?php 
+include "assets/linker/linkerJs.php" ; 
+?>
