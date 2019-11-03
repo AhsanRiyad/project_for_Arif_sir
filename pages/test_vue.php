da5bf8117b45b611137b3d756f806a53b3a615a6
Riyad Ahsan <riyad298@gmail.com>
2019-11-03 16:40:14 +0600

    .htaccess redirection added

--- pages/test_vue.php
+++ pages/test_vue.php
@@ -6,7 +6,9 @@ include "../address.php";
 include $linkerCss;
 ?>
 
-
+<div id="app">
+	<file_upload></file_upload>
+</div>
 
 
 <?php 
@@ -18,21 +20,90 @@ include $linkerJs;
 
 
 <script>
-	axios.get('<?php echo $modeltest; ?>')
-	.then(function (response) {
-    // handle successconsole.log(response.data);
-    /*console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);*/
-    console.log(response.data);
-    
-})
-	.catch(function (error) {
-    // handle error
-    console.log(error);
-})
-	.finally(function () {
-    // always executed
-});
+	var code = `<div class="container">
+	<div class="large-12 medium-12 small-12 cell">
+	<label>File
+	<input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+	</label>
+	<button class="btn btn-success" v-on:click="submitFile()">Submit</button>
+	</div>
+	</div>`;
+
+
+	Vue.component('file_upload' , {
+		template: code , 
+		data(){
+			return {
+				name_riyad: 'riyad' , 
+				file: '' ,
+			}
+		},
+		methods: {
+			submitFile: function(){
+				let formData = new FormData();
+				formData.append('file', this.file);
+
+				//console.log(this.file.size/1024/1024);
+
+				axios.post( '/single-file',
+					formData,
+					{
+						headers: {
+							'Content-Type': 'multipart/form-data'
+						}
+					}
+					).then(function(){
+						console.log('SUCCESS!!');
+					})
+					.catch(function(){
+						console.log('FAILURE!!');
+					});
+
+
+
+				},
+				handleFileUpload: function(){
+					this.file = this.$refs.file.files[0];
+				}
+			}
+		})
+
+
+
+
+
+
+
+	var registration_page = new Vue({
+		el: '#app' , 
+		data : {
+			name: 'riyad---vue',
+			
+		} , 
+		methods : {
+			
+		},
+		beforeCreate(){
+			
+		},
+		created(){
+			
+		},
+		beforeMount(){
+			
+		},
+		mounted(){
+			
+		},
+		beforeUpdated(){
+			
+		},
+		updated(){
+			
+		}
+	})
+
+
+
+
 </script>
