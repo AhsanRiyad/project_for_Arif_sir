be564e9bac67d5e73fc422ff6835f04e81321eb4
Riyad Ahsan <riyad298@gmail.com>
2019-11-18 17:26:10 +0600

    menu active color fixed

--- pages/dashboard_foot.php
+++ pages/dashboard_foot.php
@@ -1,129 +1,207 @@
 
+</div>
+
+
+
+
+<div class="col-2 order-1 dashboard_vertical_menu_height">
+	<!-- fixed vertical -->
+	<div class=" bg-dark ">
+		<div class=" bg-light d-flex justify-content-center">
+			<a href="#"><img class=" py-1" src="<?php echo $logoSrc; ?>" alt=""></a>
+
 		</div>
 
+		<?php 
+
+		if($admin__ == true){
+			?>
+			<a class="text-white" href="">
+				<div class=" 
+				<?php 
+
+				if($pageName=='dashboardHome')
+				{
+					echo 'bg-info';
+				}
+				else{
+					echo 'bg-secondary';
+				}
+				?> 
+
+				py-2 text-center my-4">
+
+				<i class="fas w_f fa-tachometer-alt"></i> View Users
+
+			</div></a>
+			<?php 
+		}
+		?>
+		
+
+		<?php 
+
+		if($admin__ == true){
+			?>
+
+			<a class="text-white" href="<?php echo $add_user; ?>">
+				<div class=" 
+
+				<?php 
+
+				if($pageName=='add_user')
+				{
+					echo 'bg-info';
+				}
+				else{
+					echo 'bg-secondary';
+				}
+				?>  
+
+				py-2 text-center my-4">
+
+				<i class="fas w_f fa-chart-line"></i> Add Users
+
+			</div></a>
+
+			<?php 
+		}
+		?>
+
+		<?php 
+		if($admin__ == true || $user__ == true){
+			?>
+			<a class="text-light" href="<?php echo $profilePage; ?>">
+				<div class="  
+				<?php 
+
+				if($pageName=='profile')
+				{
+					echo 'bg-info';
+				}
+				else{
+					echo 'bg-secondary';
+				}
+				?> 
+				py-2 text-center my-4">
+
+				<i class="fas w_f fa-user"></i> Profile
+
+			</div></a>
+			<?php 
+		}
+		?>
+
+
+		<a class="text-light" href="<?php echo $searchPage; ?>">
+			<div class="  
+			<?php 
+
+			if($pageName=='search' )
+			{
+				echo 'bg-info';
+			}
+			else{
+				echo 'bg-secondary';
+			}
+			?> 
+			py-2 text-center my-4">
+
+			<i class="fas w_f fa-user"></i> Search
+
+		</div></a>
+
+
+		<?php 
+		if($admin__ == true ){
+			?>
+			<a class="text-light" href="<?php echo $reg_req; ?>">
+				<div class="  
+				<?php 
+
+				if($pageName=='addProductBySeller')
+				{
+					echo 'bg-info';
+				}
+				else{
+					echo 'bg-secondary';
+				}
+				?> 
+				py-2 text-center my-4">
+
+				<i class="fas fa-shopping-cart"></i> User Request
+
+			</div></a>
+		<?php } ?>
+	
+
+		<?php 
+		if($admin__ == true || $user__ == true){
+			?>
+
+		<a class="text-white" href="<?php echo $privacyPage; ?>">
+			<div class=" 
+
+
+			<?php 
+
+			if($pageName=='privacy')
+			{
+				echo 'bg-info';
+			}
+			else{
+				echo 'bg-secondary';
+			}
+			?>  
 
 
 
-		<div class="col-2 order-1 dashboard_vertical_menu_height">
-			<!-- fixed vertical -->
-			<div class=" bg-dark ">
-				<div class=" bg-light d-flex justify-content-center">
-					<a href=><img class=" py-1" src="<?php echo $logoSrc; ?>" alt=""></a>
+			bg-secondary py-2 text-center my-4">
 
-				</div>
-				<a class="text-white" href=>
-					<div class=" 
-					<?php 
+			<i class="fas w_f fa-envelope"></i> Privacy
 
-					if($pageName=='dashboardHome')
-					{
-						echo 'bg-info';
-					}
-					else{
-						echo 'bg-secondary';
-					}
-					?> 
+		</div></a>
 
-					py-2 text-center my-4">
+	<?php } ?>
 
-					<i class="fas w_f fa-tachometer-alt"></i> View Users
+	
+		<?php 
+		if($admin__ == true || $user__ == true){
+			?>
+
+
+
+		<a class="text-white" href="<?=  $galleryPage; ?>">
+			<div class=" 
+			
+
+			<?php 
+
+			if($pageName=='gallery')
+			{
+				echo 'bg-info';
+			}
+			else{
+				echo 'bg-secondary';
+			}
+			?>  
+
+			
+			  py-2 text-center my-4">
+
+				<i class="fab fa-product-hunt"></i> Gallery
+
+			</div></a>
+
+				<?php } ?>
+
+			<a class="text-white" href="<?php echo $loginPage; ?>">
+				<div class="  bg-danger py-2 text-center my-4">
+
+					<i class="fas fa-sign-out-alt"></i>Sign Out
 
 				</div></a>
 
-				<a class="text-white" href="<?php echo $add_user; ?>">
-					<div class=" bg-secondary py-2 text-center my-4">
-
-						<i class="fas w_f fa-chart-line"></i> Add Users
-
-					</div></a>
-
-
-					<a class="text-light" href="<?php echo $profilePage; ?>">
-						<div class="  
-						<?php 
-
-						if($pageName=='dashboardProfile' || $pageName=='dashboardProfileUpdate')
-						{
-							echo 'bg-info';
-						}
-						else{
-							echo 'bg-secondary';
-						}
-						?> 
-						py-2 text-center my-4">
-
-						<i class="fas w_f fa-user"></i> Profile
-
-					</div></a>
-
-					<a class="text-light" href="<?php echo $searchPage; ?>">
-						<div class="  
-						<?php 
-
-						if($pageName=='dashboardProfile' || $pageName=='dashboardProfileUpdate')
-						{
-							echo 'bg-info';
-						}
-						else{
-							echo 'bg-secondary';
-						}
-						?> 
-						py-2 text-center my-4">
-
-						<i class="fas w_f fa-user"></i> Search
-
-					</div></a>
-
-
-					<a class="text-light" href="<?php echo $reg_req; ?>">
-						<div class="  
-						<?php 
-
-						if($pageName=='addProductBySeller')
-						{
-							echo 'bg-info';
-						}
-						else{
-							echo 'bg-secondary';
-						}
-						?> 
-						py-2 text-center my-4">
-
-						<i class="fas fa-shopping-cart"></i> User Request
-
-					</div></a>
-
-
-					<a class="text-white" href="<?php echo $privacyPage; ?>">
-						<div class="  bg-secondary py-2 text-center my-4">
-
-							<i class="fas w_f fa-envelope"></i> Privacy
-
-						</div></a>
-
-
-						<a class="text-white" href="<?=  $galleryPage; ?>">
-							<div class="  bg-secondary py-2 text-center my-4">
-
-								<i class="fab fa-product-hunt"></i> Gallery
-
-							</div></a>
-
-
-							<a class="text-white" href=>
-								<div class="  bg-success py-2 text-center my-4">
-
-									<i class="fas fa-shopping-cart"></i> Back to Shop Page
-
-								</div></a>
-
-								<a class="text-white" href="<?php echo $loginPage; ?>">
-									<div class="  bg-danger py-2 text-center my-4">
-
-										<i class="fas fa-sign-out-alt"></i>Sign Out
-
-									</div></a>
-
-						</div>		
-						</div>
-					</div>
+			</div>		
+		</div>
+	</div>
