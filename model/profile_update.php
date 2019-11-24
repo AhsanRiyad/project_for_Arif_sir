bdd3d6dc48a83ce9e721cf654121011603145375
Riyad Ahsan <riyad298@gmail.com>
2019-11-24 19:24:45 +0600

    Revert "Revert "unstable""
    This reverts commit 2724e467e28613dcf7f74a2b9e86d69a8ec5e999.

--- model/profile_update.php
+++ model/profile_update.php
@@ -34,6 +34,65 @@ $d1 = json_decode($data);
 //echo $d1->purpose;
 
 
+function verify_change_request($user_id){
+
+
+$conn = get_mysqli_connection();
+$sql = "select `full_name`, `mobile`, `institution_id`,`nid_or_passport`, `fathers_name`, `mother_name`, `spouse_name`, `number_of_children`, `profession`, `designation`, `institution`, `blood_group`, `date_of_birth`, `present_line1`, `present_district`, `present_post_code`, `present_country`, `parmanent_line1`,  `parmanent_district`, `parmanent_post_code`, `parmanent_country`  from all_info_together where  id = ".$user_id." ";
+
+$result = mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+  }
+
+$arrayValueString =  implode(',' ,$row);
+$arrayKeyString =  implode(',' , array_keys($row));
+$newArrayString = $arrayKeyString .'@#$'.$arrayValueString;
+$arrayTogether = explode('@#$' , $newArrayString);
+$array1keyString = $arrayTogether[0];
+$array2ValueString = $arrayTogether[1];
+//final pair1
+$arrayKey_tobeUpdated =  $stringToArrayKey = explode(',' , $array1keyString);
+$arrayValue_tobeUpdated = $stringToArrayValue = explode(',' , $array2ValueString);
+$arrayAssoc;
+$i = 0;
+for($i =0; $i<count($stringToArrayKey); $i++){
+  
+   $arrayAssoc[$stringToArrayKey[$i]] = $stringToArrayValue[$i];
+}
+$sql = "select last_verified_info from all_info_together where  id = ".$user_id." ";
+
+$result = mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+  }
+
+
+$array =  explode('@#$' , $row['last_verified_info']);
+//desired array
+$arrayKey_from_database =  $stringToArrayKey_from_verified_info = explode(',' , $array[0] );
+$arrayValue_from_database = $stringToArrayValue_from_verified_info = explode(',' , $array[1] );
+$arrayNew = [];
+for($i=0; $i<count($arrayValue_from_database) ; $i++){
+  if($arrayValue_from_database[$i] != $arrayValue_tobeUpdated[$i]){
+    $arrayNew[$arrayKey_from_database[$i]] = [$arrayValue_tobeUpdated[$i], $arrayValue_from_database[$i]] ; 
+  }
+}
+
+if(count($arrayNew)==0){
+
+  $sql = "update all_info_together set change_request = 'not_requested'  where id = ".$user_id." ";
+  mysqli_query($conn , $sql);
+
+}
+
+ $conn->close();
+}
+
+
+
+
+
 if($d1->purpose == 'basic'){
   $full_name= $d1->full_name;
   $mobile= $d1->mobile;
@@ -49,25 +108,28 @@ if($d1->purpose == 'basic'){
   $result = mysqli_query($conn, $sql);
   if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
- }
+  }
 
- $arrayValueString =  implode(',' ,$row);
- $arrayKeyString =  implode(',' , array_keys($row));
- $verified_info = $arrayKeyString .'@#$'.$arrayValueString;
-
-
- $sql = "call update_profile_basic(?,?,?,?,?,?,?,?,@result)";
- $stmt = $conn->prepare($sql);
- $stmt->bind_param('isssssss' , $id__ , $verified_info , $full_name, $mobile , $institution_id , $blood_group , $nid_or_passport , $dob );
- $stmt->execute();
- $stmt->close();
+  $arrayValueString =  implode(',' ,$row);
+  $arrayKeyString =  implode(',' , array_keys($row));
+  $verified_info = $arrayKeyString .'@#$'.$arrayValueString;
 
 
- $sql = 'select @result as st'; 
- $result = mysqli_query($conn, $sql);
- $row = mysqli_fetch_assoc($result);
+  $sql = "call update_profile_basic(?,?,?,?,?,?,?,?,@result)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('isssssss' , $id__ , $verified_info , $full_name, $mobile , $institution_id , $blood_group , $nid_or_passport , $dob );
+  $stmt->execute();
+  $stmt->close();
 
- echo $row['st'];
+
+  $sql = 'select @result as st'; 
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+
+  verify_change_request($id__);
+
+
+  echo $row['st'];
 
 
 
@@ -85,26 +147,29 @@ if($d1->purpose == 'basic'){
   $result = mysqli_query($conn, $sql);
   if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
- }
+  }
 
- $arrayValueString =  implode(',' ,$row);
- $arrayKeyString =  implode(',' , array_keys($row));
- $verified_info = $arrayKeyString .'@#$'.$arrayValueString;
-
-
+  $arrayValueString =  implode(',' ,$row);
+  $arrayKeyString =  implode(',' , array_keys($row));
+  $verified_info = $arrayKeyString .'@#$'.$arrayValueString;
 
 
- $sql = "call update_profile_personal(?,?,?,?,?,?,?,?,?,@result)";
- $stmt = $conn->prepare($sql);
- $stmt->bind_param('issssisss' , $id__ , $verified_info , $fathers_name , $mothers_name, $spouse_name , $number_of_children , $profession , $workplace_or_institution , $designation  );
 
- $stmt->execute();
- $stmt->close();
- $sql = 'select @result as st'; 
- $result = mysqli_query($conn, $sql);
- $row = mysqli_fetch_assoc($result);
 
- echo $row['st'];
+  $sql = "call update_profile_personal(?,?,?,?,?,?,?,?,?,@result)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('issssisss' , $id__ , $verified_info , $fathers_name , $mothers_name, $spouse_name , $number_of_children , $profession , $workplace_or_institution , $designation  );
+
+  $stmt->execute();
+  $stmt->close();
+  $sql = 'select @result as st'; 
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+
+  verify_change_request($id__);
+
+
+  echo $row['st'];
 
 
 
@@ -125,25 +190,28 @@ if($d1->purpose == 'basic'){
   $result = mysqli_query($conn, $sql);
   if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
- }
+  }
 
- $arrayValueString =  implode(',' ,$row);
- $arrayKeyString =  implode(',' , array_keys($row));
- $verified_info = $arrayKeyString .'@#$'.$arrayValueString;
+  $arrayValueString =  implode(',' ,$row);
+  $arrayKeyString =  implode(',' , array_keys($row));
+  $verified_info = $arrayKeyString .'@#$'.$arrayValueString;
 
 
- $sql = "call update_profile_address(?,?,?,?,?,?,?,?,?,?,@result)";
- $stmt = $conn->prepare($sql);
- $stmt->bind_param('isssssssss' , $id__ , $verified_info ,$present_line1 , $present_district , $present_post_code , $present_country , $permanent_line1 , $permanent_district , $permanent_post_code , $permanent_country );
- $stmt->execute();
- $stmt->close();
+  $sql = "call update_profile_address(?,?,?,?,?,?,?,?,?,?,@result)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('isssssssss' , $id__ , $verified_info ,$present_line1 , $present_district , $present_post_code , $present_country , $permanent_line1 , $permanent_district , $permanent_post_code , $permanent_country );
+  $stmt->execute();
+  $stmt->close();
 
- $sql = 'select @result as st'; 
- $result = mysqli_query($conn, $sql);
- $row = mysqli_fetch_assoc($result);
+  $sql = 'select @result as st'; 
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
 
- $conn->close();
- echo $row['st'];
+  verify_change_request($id__);
+
+
+  $conn->close();
+  echo $row['st'];
 
 
 
@@ -181,11 +249,11 @@ if($d1->purpose == 'basic'){
   $result = mysqli_query($conn, $sql);
   if (mysqli_num_rows($result) > 0) {
    $row = mysqli_fetch_assoc($result);
-}
+ }
 
-if($row['count'] > 0){
+ if($row['count'] > 0){
    echo 'email already exists';
-}else{
+ }else{
    $otp = rand(1000,9999);
    $mailto = $email1;
    $mailSub = 'OTP for Changine Email';
@@ -198,14 +266,14 @@ if($row['count'] > 0){
 
    if(!$mail->Send())
    {
-      echo 'server_problem';
-   }else{
+    echo 'server_problem';
+  }else{
 
-      $sql = "update all_info_together set otp = '".$otp."'  where id='".$id__."' ";
-      $result = mysqli_query($conn, $sql);
-      echo 'success';
+    $sql = "update all_info_together set otp = '".$otp."'  where id='".$id__."' ";
+    $result = mysqli_query($conn, $sql);
+    echo 'success';
 
-   }
+  }
 
 }
 
@@ -220,17 +288,17 @@ if($row['count'] > 0){
   $sql = "call update_profile_email(".$id__.", '".$email__."', '".$email."' , '".$otp."' , @result)";
 
    // echo $sql;
-   $result = mysqli_query($conn, $sql);
+  $result = mysqli_query($conn, $sql);
 
-   $sql = "select @result as rs";
-   $result = mysqli_query($conn, $sql);
+  $sql = "select @result as rs";
+  $result = mysqli_query($conn, $sql);
 
-   if (mysqli_num_rows($result) > 0) {
+  if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
     echo $row['rs'];
- }
+  }
 
-   
+
   //$result = mysqli_query($conn, $sql);
   //$sql = "select @result as rs";
  //  if (mysqli_num_rows($result) > 0) {
@@ -265,7 +333,7 @@ if($row['count'] > 0){
 
   if($row['st']=='no_email_found'){
     echo $row['st'];
- }else if($row['st']=='crypto_added'){
+  }else if($row['st']=='crypto_added'){
 
 
     $mailto = $email1;
@@ -282,13 +350,13 @@ if($row['count'] > 0){
     if(!$mail->Send())
     {
       echo 'server_problem';
-   }
-   else
-   {
+    }
+    else
+    {
       echo $row['st'];
-   }
+    }
 
-}
+  }
 
 
 
@@ -301,10 +369,10 @@ if($row['count'] > 0){
   if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
     echo json_encode($row);
- } else {
+  } else {
     echo "0 results";
- }
- mysqli_close($conn);
+  }
+  mysqli_close($conn);
 
 
 	// $i = 0;
@@ -364,16 +432,16 @@ if($row['count'] > 0){
   $sql = '';
   if($user_type == 'admin'){
     $sql = "call user_request(? , @result)";
- }else if($user_type == 'user'){
+  }else if($user_type == 'user'){
     $sql = "update all_info_together set completeness = 100 where id = (?)";
- }
+  }
 
- $stmt = $conn->prepare($sql);
- $stmt->bind_param('i' , $id__ );
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('i' , $id__ );
 
- $stmt->execute();
- $stmt->close();
- $conn->close();
+  $stmt->execute();
+  $stmt->close();
+  $conn->close();
 
 }else if($d1->purpose == 'send_otp'){
 
@@ -409,12 +477,12 @@ if($row['count'] > 0){
     if(!$mail->Send())
     {
       echo 'server_problem';
-   }
-   else
-   {
+    }
+    else
+    {
       echo $row['st'];
-   }
-};
+    }
+  };
 
 	// $i = 0;
 // echo json_encode(var_dump($row));
--- model/reg_req.php
+++ model/reg_req.php
@@ -82,7 +82,7 @@ if($d2->purpose=='get_data'){
 }else if($d2->purpose=='get_change_req_data'){
 $conn = get_mysqli_connection();
 $user_id = $d2->user_id;
-$sql = "select `full_name`, `mobile`, `institution_id`,`nid_or_passport`, `fathers_name`, `mother_name`, `spouse_name`, `number_of_children`, `profession`, `designation`, `institution`, `blood_group`, `date_of_birth`, `present_line1`, `present_district`, `present_post_code`, `present_country`, `parmanent_line1`,  `parmanent_district`, `parmanent_post_code`, `parmanent_country` from all_info_together where  id = ".$user_id." ";
+$sql = "select `full_name`, `mobile`, `institution_id`,`nid_or_passport`, `fathers_name`, `mother_name`, `spouse_name`, `number_of_children`, `profession`, `designation`, `institution`, `blood_group`, `date_of_birth`, `present_line1`, `present_district`, `present_post_code`, `present_country`, `parmanent_line1`,  `parmanent_district`, `parmanent_post_code`, `parmanent_country`  from all_info_together where  id = ".$user_id." ";
 
 $result = mysqli_query($conn, $sql);
 	if (mysqli_num_rows($result) > 0) {
@@ -134,7 +134,7 @@ else if($d2->purpose=='reject_user'){
 	// mysqli_close($conn);
 	$conn = get_mysqli_connection();
 	//mysqli_close($conn);
-	$sql = "UPDATE all_info_together SET status ='rejected' WHERE id = ".$user_id." ";
+	$sql = "UPDATE all_info_together SET status ='rejected' , completeness = 80  WHERE id = ".$user_id." ";
 	$result = mysqli_query($conn, $sql);
 	mysqli_close($conn);
 	
