0933f50e8439aabb641d61a34887044f6b141cb0
Riyad Ahsan <riyad298@gmail.com>
2019-11-25 20:26:13 +0600

    orgainized serially

--- assets/js/vue_file/forgot.php
+++ assets/js/vue_file/forgot.php
@@ -18,6 +18,97 @@
 
 
 
+
+
+	var forgot_password = `
+	
+	<div class="container-fluid">
+	<div class="row justify-content-xl-center justify-content-md-center _background ">
+	<div class="col-12 col-xl-4 col-md-4 align-self-center ">
+	<div class="container margin_">
+	<div class="row py-4 ">
+
+
+
+
+
+	<p class="text-dark h4" id="login_id">
+	Forgot Password?
+	<br>
+	Please enter your email to recover..
+	<br>
+	<span v-show="login_status=='no_email_found'" class="red--text"> Account not found </span>
+	<span v-show="login_status=='invalid_email'" class="red--text"> {{ login_status }} </span>
+	<span v-show="login_status== 'crypto_added' " class="green--text"> recovery link sent to your email </span>
+
+	<span v-show="invalid_link=='invalid_link'" class="red--text"> link expired/invalid, try again </span>
+
+	<span v-show="login_status=='link_sent_successfully'" class="red--text"> link expired/invalid, try again </span>
+	</p>
+	<span v-show="login_status=='server_problem'" class="red--text"> Email server problem , try later. </span>
+	</p>
+
+
+
+	</div>
+
+	<div class="row justify-content-xl-center bg-white py-5 mb-5">
+
+	<div class="col-12 col-xl-9 ">
+
+	<div class="form-group">
+	<label for="exampleInputEmail1" ><small id="idExampleInputEmail1Small">Email address*
+
+	<span v-show="email_validity == 'valid'" class="text-success"> {{ email_validity }} </span>
+	<span v-show="email_validity == 'invalid'" class="text-danger"> {{ email_validity }}  </span>
+
+
+
+	
+
+	</small>
+	<br>
+
+
+
+	</label>
+	<input @keyup="onChangeValidity('email')" v-model='email' type="text" class="form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"
+	value=""
+	>
+	</div>
+
+	<v-btn color="success" :loading = 'loading' @click="submit" type="submit" name="submit" value="submit" class="btn btn-success rounded-0 mb-1 w-100 py-2">Recover Password</v-btn >
+
+	<!-- <button type="submit" name="submit" value="submit" class="btn btn-info rounded-0 mb-1 w-100 py-2">Back to Login</button> -->
+
+	<br>
+
+	<div class="row justify-content-end">
+	<div class="col-lg-auto">
+	<a v-bind:href="address.loginPage" class="">Go Back to Login</a>
+	</div>
+	</div>
+
+
+
+
+
+	</div>
+
+
+
+	</div>
+	</div>
+	</div>
+	</div>
+
+
+	
+
+	</div>
+	
+	`;
+	
 	
 
 
@@ -251,97 +342,6 @@
 
 
 
-
-
-	var forgot_password = `
-	
-	<div class="container-fluid">
-	<div class="row justify-content-xl-center justify-content-md-center _background ">
-	<div class="col-12 col-xl-4 col-md-4 align-self-center ">
-	<div class="container margin_">
-	<div class="row py-4 ">
-
-
-
-
-
-	<p class="text-dark h4" id="login_id">
-	Forgot Password?
-	<br>
-	Please enter your email to recover..
-	<br>
-	<span v-show="login_status=='no_email_found'" class="red--text"> Account not found </span>
-	<span v-show="login_status=='invalid_email'" class="red--text"> {{ login_status }} </span>
-	<span v-show="login_status== 'crypto_added' " class="green--text"> recovery link sent to your email </span>
-
-	<span v-show="invalid_link=='invalid_link'" class="red--text"> link expired/invalid, try again </span>
-
-	<span v-show="login_status=='link_sent_successfully'" class="red--text"> link expired/invalid, try again </span>
-	</p>
-	<span v-show="login_status=='server_problem'" class="red--text"> Email server problem , try later. </span>
-	</p>
-
-
-
-	</div>
-
-	<div class="row justify-content-xl-center bg-white py-5 mb-5">
-
-	<div class="col-12 col-xl-9 ">
-
-	<div class="form-group">
-	<label for="exampleInputEmail1" ><small id="idExampleInputEmail1Small">Email address*
-
-	<span v-show="email_validity == 'valid'" class="text-success"> {{ email_validity }} </span>
-	<span v-show="email_validity == 'invalid'" class="text-danger"> {{ email_validity }}  </span>
-
-
-
-	
-
-	</small>
-	<br>
-
-
-
-	</label>
-	<input @keyup="onChangeValidity('email')" v-model='email' type="text" class="form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"
-	value=""
-	>
-	</div>
-
-	<v-btn color="success" :loading = 'loading' @click="submit" type="submit" name="submit" value="submit" class="btn btn-success rounded-0 mb-1 w-100 py-2">Recover Password</v-btn >
-
-	<!-- <button type="submit" name="submit" value="submit" class="btn btn-info rounded-0 mb-1 w-100 py-2">Back to Login</button> -->
-
-	<br>
-
-	<div class="row justify-content-end">
-	<div class="col-lg-auto">
-	<a v-bind:href="address.loginPage" class="">Go Back to Login</a>
-	</div>
-	</div>
-
-
-
-
-
-	</div>
-
-
-
-	</div>
-	</div>
-	</div>
-	</div>
-
-
-	
-
-	</div>
-	
-	`;
-	
 
 	Vue.component('forgot_password' , {
 		template: forgot_password,
