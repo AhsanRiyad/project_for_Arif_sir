441b5fec3a0f72c086bd7ecb1d09dee2367ce5a5
Riyad Ahsan <riyad298@gmail.com>
2019-12-01 23:26:30 +0600

    cors added , stable

--- address.php
+++ address.php
@@ -6,6 +6,39 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
+
+
+cors() ;
+
+function cors() {
+
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+}
+
+
+
+
+
 session_start();
 
 $APP_ROOT = $_SERVER['DOCUMENT_ROOT']."/project_for_Arif_sir/";
